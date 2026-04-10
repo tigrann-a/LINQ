133c0d36c4e4995aef48e7f336cece7d003d7af0
School <School@DESKTOP-RR0AV5V>
2026-04-10 14:14:03 +0400

    Adding Self-Training code

--- LINQ_20260408/Program.cs
+++ LINQ_20260408/Program.cs
@@ -1,57 +1,56 @@
 ﻿using LINQ_20260408;
 using System.IO;
 
-string path = @"C:\Users\School\Pictures\Screenshots";
-ShowLargeFilesWithoutLinq(path);
+//string path = @"C:\Users\School\Pictures\Screenshots";
+//ShowLargeFilesWithoutLinq(path);
 
-static void ShowLargeFilesWithoutLinq(string path)
-{
+//static void ShowLargeFilesWithoutLinq(string path)
+//{
     //DirectoryInfo directory = new DirectoryInfo(path);
     //FileInfo[] files = directory.GetFiles();
 
-    //for (int j = 0; j < files.Length; j++)
-    //{
-    //    long min = files[0].Length;
-    //    for (int i = 0; i < files.Length - 1; i++)
-    //    {
-    //        if (min > files[i + 1].Length && !lengths.Contains(files[i + 1].Length))
-    //        {
-    //            min = files[i + 1].Length;
-    //        }
-    //        //Console.WriteLine(files[i].Length);
 
-    //    }
-    //    lengths[j] = min;
-    //}
+    //var query = from file in new DirectoryInfo(path).GetFiles()
+    //            orderby file.Length descending
+    //            select file;
 
-    //for (int a = 0; a < lengths.Length; a++)
-    //{
-    //    Console.WriteLine(lengths[a]);
-    //}
-
-    //foreach (FileInfo file in files)
-    //{
-    //    Array.Sort(files, new FileInfoComparer());
-    //    Console.WriteLine($"{file.Name} : {file.Length}");
-    //}
-
-
-        //var query = from file in new DirectoryInfo(path).GetFiles()
-        //            orderby file.Length descending
-        //            select file;
-
-        var query = new DirectoryInfo(path).GetFiles().OrderByDescending(f => f.Length).Take(5);
+    //var query = new DirectoryInfo(path).GetFiles().OrderByDescending(f => f.Length).Take(5);
 
     //foreach (var file in query.Take(5))
     //{
     //    Console.WriteLine($"{file.Name,-50} : {file.Length,10:NO}");
     //}
 
-    foreach (var file in query)
-    {
-        Console.WriteLine($"{file.Name,-50} : {file.Length,10:NO}");
-    }
+    //foreach (var file in query)
+    //{
+    //    Console.WriteLine($"{file.Name,-50} : {file.Length,10:NO}");
+    //}
+//}
 
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var eventNumbers = numbers.Where(n => n % 2 == 0);
+
+foreach (var number in eventNumbers)
+{
+    Console.WriteLine(number);
 }
 
+Console.WriteLine();
+
+var first = numbers.First();
+
+Console.WriteLine($"The first number is: {first}");
+
+Console.WriteLine();
+
+var hasBigNumber = numbers.Any(n => n > 100);
+
+Console.WriteLine($"Does numbers array have big numbers: {hasBigNumber}");
+
+Console.WriteLine();
+
+var total = numbers.Sum();
+
+Console.WriteLine($"The sum of numbers is: {total}");
 
