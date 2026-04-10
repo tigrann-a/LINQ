0488130f337912d75649cfc63ee57dbace2e1e06
School <School@DESKTOP-RR0AV5V>
2026-04-10 14:44:52 +0400

    Assing Bubble sorting logic

--- LINQ_20260408/Program.cs
+++ LINQ_20260408/Program.cs
@@ -1,14 +1,35 @@
 ﻿using LINQ_20260408;
 using System.IO;
 
-//string path = @"C:\Users\School\Pictures\Screenshots";
-//ShowLargeFilesWithoutLinq(path);
+string path = @"C:\Users\School\Pictures\Screenshots";
+ShowLargeFilesWithoutLinq(path);
 
-//static void ShowLargeFilesWithoutLinq(string path)
-//{
-    //DirectoryInfo directory = new DirectoryInfo(path);
-    //FileInfo[] files = directory.GetFiles();
+static void ShowLargeFilesWithoutLinq(string path)
+{
+    DirectoryInfo directory = new DirectoryInfo(path);
+    FileInfo[] files = directory.GetFiles();
 
+    for(int i = 0; i < files.Length - 1; i++)
+    {
+        for (int j = 0; j < files.Length - i - 1; j++)
+        {
+            if (files[j].Length < files[j + 1].Length)
+            {
+                FileInfo temp = files[j];
+                files[j] = files[j + 1];
+                files[j + 1] = temp;
+            }
+        }
+    }
+
+    int k = 1;
+    foreach(var file in files)
+    {
+        if(k <= 5)
+            Console.WriteLine($"{file.Length}");
+        k++;
+    }
+        
 
     //var query = from file in new DirectoryInfo(path).GetFiles()
     //            orderby file.Length descending
@@ -25,32 +46,32 @@ using System.IO;
     //{
     //    Console.WriteLine($"{file.Name,-50} : {file.Length,10:NO}");
     //}
-//}
-
-int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-var eventNumbers = numbers.Where(n => n % 2 == 0);
-
-foreach (var number in eventNumbers)
-{
-    Console.WriteLine(number);
 }
 
-Console.WriteLine();
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-var first = numbers.First();
+//var eventNumbers = numbers.Where(n => n % 2 == 0);
 
-Console.WriteLine($"The first number is: {first}");
+//foreach (var number in eventNumbers)
+//{
+//    Console.WriteLine(number);
+//}
 
-Console.WriteLine();
+//Console.WriteLine();
 
-var hasBigNumber = numbers.Any(n => n > 100);
+//var first = numbers.First();
 
-Console.WriteLine($"Does numbers array have big numbers: {hasBigNumber}");
+//Console.WriteLine($"The first number is: {first}");
 
-Console.WriteLine();
+//Console.WriteLine();
 
-var total = numbers.Sum();
+//var hasBigNumber = numbers.Any(n => n > 100);
 
-Console.WriteLine($"The sum of numbers is: {total}");
+//Console.WriteLine($"Does numbers array have big numbers: {hasBigNumber}");
+
+//Console.WriteLine();
+
+//var total = numbers.Sum();
+
+//Console.WriteLine($"The sum of numbers is: {total}");
 
